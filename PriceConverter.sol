c82d3b4b4e495c9b2ce4bbc1b548ed948236d89e
Michele Picozzi <59815609+picozzimichele@users.noreply.github.com>
2022-11-28 18:19:08 +0000

    Create PriceConverter.sol

---
+++ PriceConverter.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    function getPrice() internal view returns(uint256) {
+        //ABI
+        //Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (
+            ,
+            /*uint80 roundID*/ int price /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/,
+            ,
+            ,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+        //price has 8decimal numbers
+
+    }
+
+    function getVersion() internal view returns(uint256) {
+         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+         return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+}
